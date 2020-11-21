1fe27593517a0fb7a7e12463e51b0d1c6a7e0647
BLemine <baillahiamine@gmail.com>
2020-11-21 16:32:52 +0100

    refactoring

--- client/src/services/index.js
+++ client/src/services/index.js
@@ -1,5 +1,5 @@
 import Axios from "axios";
 export const Instance =Axios.create({
-    baseURL:"https://medium-movies-trending-server.herokuapp.com",
+    baseURL:"http://localhost:2020",
     data:{}
 })
